38cd1d308b981f1e918a4c94cd2a25781affa2f4
Quentin Thébault <quentin.thebault@defenso.fr>
2026-08-31 04:49:15 +0200

    bhyve: reinitialise swtpm's TPM at startup
    bhyve does not currently reinitialise swtpm on startup, which results in
    PCRs being carried over across reboots. This means that any guest VM
    implementing measured boot controls, such as Windows with BitLocker and
    Secure Boot enabled, will consider the system compromised and refuse to
    start or ask for a recovery key.
    
    swtpm has a control channel that allows us to send commands for that
    purpose: CMD_INIT shuts the TPM down and starts it again. When swptpm
    has been configured with flags startup-clear, it clears the PCRs, like a
    cold boot on real hardware.
    
    Connect a second socket when the new ctrl_path option is set and send
    CMD_INIT there. Without the option nothing changes.
    
    MFC after:	2 weeks
    Sponsored by: 	Defenso
    
    Signed-off-by: Quentin Thébault <quentin.thebault@defenso.fr>

--- usr.sbin/bhyve/tpm_emul_swtpm.c
+++ usr.sbin/bhyve/tpm_emul_swtpm.c
@@ -3,9 +3,12 @@
  *
  * Copyright (c) 2024 Hans Rosenfeld
  * Author: Hans Rosenfeld <rosenfeld@grumpf.hope-2000.org>
+ * Copyright (c) 2026 Defenso SAS
+ * Author: Quentin Thébault <quentin.thebault@defenso.fr>
  */
 
 #include <sys/types.h>
+#include <sys/endian.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 
@@ -24,7 +27,80 @@
 
 struct tpm_swtpm {
 	int fd;
+	int ctrl_fd;
 };
+
+#define	SWTPM_CMD_INIT		2
+
+struct swtpm_init_req {
+	uint32_t cmd;
+	uint32_t init_flags;
+};
+
+static int
+tpm_swtpm_ctrl_init(struct tpm_swtpm *tpm, const char *ctrl_path)
+{
+	struct sockaddr_un ctrl_addr;
+	struct swtpm_init_req req;
+	uint32_t res;
+	ssize_t len;
+
+	tpm->ctrl_fd = socket(PF_UNIX, SOCK_STREAM | SOCK_CLOEXEC, 0);
+	if (tpm->ctrl_fd < 0) {
+		warnx("%s: unable to open tpm control socket", __func__);
+		return (ENOENT);
+	}
+
+	bzero(&ctrl_addr, sizeof (ctrl_addr));
+	ctrl_addr.sun_family = AF_UNIX;
+	if (strlcpy(ctrl_addr.sun_path, ctrl_path,
+	    sizeof (ctrl_addr.sun_path)) >= sizeof (ctrl_addr.sun_path)) {
+		warnx("%s: tpm control socket path \"%s\" is too long "
+		    "(limit %zu)", __func__, ctrl_path,
+		    sizeof (ctrl_addr.sun_path) - 1);
+		return (ENAMETOOLONG);
+	}
+
+	if (connect(tpm->ctrl_fd, (struct sockaddr *)&ctrl_addr,
+	    sizeof (ctrl_addr)) == -1) {
+		warnx("%s: unable to connect to tpm control socket \"%s\"",
+		    __func__, ctrl_path);
+		return (ENOENT);
+	}
+
+	req.cmd = htobe32(SWTPM_CMD_INIT);
+	req.init_flags = 0;
+
+	len = send(tpm->ctrl_fd, &req, sizeof (req), MSG_NOSIGNAL);
+	if (len == -1) {
+		warn("%s: failed to send CMD_INIT", __func__);
+		return (errno);
+	}
+	if (len != (ssize_t)sizeof (req)) {
+		warnx("%s: short write sending CMD_INIT (sent %zd of %zu)", __func__,
+		    len, sizeof (req));
+		return (EIO);
+	}
+
+	len = recv(tpm->ctrl_fd, &res, sizeof (res), MSG_WAITALL);
+	if (len == -1) {
+		warn("%s: no reply to CMD_INIT", __func__);
+		return (errno);
+	}
+	if (len != (ssize_t)sizeof (res)) {
+		warnx("%s: truncated reply to CMD_INIT (read %zd of %zu)", __func__,
+		    len, sizeof (res));
+		return (EIO);
+	}
+
+	if (res != 0) {
+		warnx("%s: swtpm failed to initialise the TPM: 0x%x", __func__,
+		    be32toh(res));
+		return (EIO);
+	}
+
+	return (0);
+}
 
 struct tpm_resp_hdr {
 	uint16_t tag;
@@ -36,14 +112,16 @@ static int
 tpm_swtpm_init(void **sc, nvlist_t *nvl)
 {
 	struct tpm_swtpm *tpm;
-	const char *path;
+	const char *path, *ctrl_path;
 	struct sockaddr_un tpm_addr;
+	int error;
 
 	tpm = calloc(1, sizeof (struct tpm_swtpm));
 	if (tpm == NULL) {
 		warnx("%s: failed to allocate tpm_swtpm", __func__);
 		return (ENOMEM);
 	}
+	tpm->ctrl_fd = -1;
 
 	path = get_config_value_node(nvl, "path");
 	if (path == NULL) {
@@ -59,13 +137,26 @@ tpm_swtpm_init(void **sc, nvlist_t *nvl)
 
 	bzero(&tpm_addr, sizeof (tpm_addr));
 	tpm_addr.sun_family = AF_UNIX;
-	strlcpy(tpm_addr.sun_path, path, sizeof (tpm_addr.sun_path) - 1);
+	if (strlcpy(tpm_addr.sun_path, path,
+	    sizeof (tpm_addr.sun_path)) >= sizeof (tpm_addr.sun_path)) {
+		warnx("%s: tpm data socket path \"%s\" is too long "
+		    "(limit %zu)", __func__, path,
+		    sizeof (tpm_addr.sun_path) - 1);
+		return (ENAMETOOLONG);
+	}
 
 	if (connect(tpm->fd, (struct sockaddr *)&tpm_addr, sizeof (tpm_addr)) ==
 	    -1) {
 		warnx("%s: unable to connect to tpm socket \"%s\"", __func__,
 		    path);
 		return (ENOENT);
+	}
+
+	ctrl_path = get_config_value_node(nvl, "ctrl_path");
+	if (ctrl_path != NULL) {
+		error = tpm_swtpm_ctrl_init(tpm, ctrl_path);
+		if (error != 0)
+			return (error);
 	}
 
 	*sc = tpm;
@@ -93,7 +184,7 @@ tpm_swtpm_execute_cmd(void *sc, void *cmd, uint32_t cmd_size, void *rsp,
 	if (len != cmd_size) {
 		warn("%s: cmd write failed (bytes written: %zd / %d)", __func__,
 		    len, cmd_size);
-		return (EFAULT);
+		return (EIO);
 	}
 
 	len = recv(tpm->fd, rsp, rsp_size, 0);
@@ -102,7 +193,7 @@ tpm_swtpm_execute_cmd(void *sc, void *cmd, uint32_t cmd_size, void *rsp,
 	if (len < (ssize_t)sizeof(struct tpm_resp_hdr)) {
 		warn("%s: rsp read failed (bytes read: %zd / %d)", __func__,
 		    len, rsp_size);
-		return (EFAULT);
+		return (EIO);
 	}
 
 	return (0);
@@ -119,6 +210,8 @@ tpm_swtpm_deinit(void *sc)
 
 	if (tpm->fd >= 0)
 		close(tpm->fd);
+	if (tpm->ctrl_fd >= 0)
+		close(tpm->ctrl_fd);
 
 	free(tpm);
 }
